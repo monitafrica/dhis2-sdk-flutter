12a84e1cf48556df287379748dc1b6172cd23af7
Vincent Minde <vincentminde@gmail.com>
2020-09-08 12:47:40 +0300

    Improve date utils

--- lib/core/utils/date-utils.dart
+++ lib/core/utils/date-utils.dart
@@ -1,7 +1,16 @@
 
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 String getISODate(DateTime date){
   return '${date.toIso8601String().substring(0,23)}Z';
 }
 String getCurrentISODate(){
   return getISODate(DateTime.now());
+}
+String formatTimeOfDay(TimeOfDay timeOfDay) {
+  final now = new DateTime.now();
+  final dt = DateTime(now.year, now.month, now.day, timeOfDay.hour, timeOfDay.minute);
+  final format = DateFormat.jm();  //"6:00 AM"
+  return format.format(dt);
 }
